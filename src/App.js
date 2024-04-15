579119736a67a57ca4a742f8646bcaab7ce2d19b
Collin Barlage <collin.barlage@gopuff.com>
2024-04-15 16:54:38 -0400

    redirect

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import './App.css';
 import { TestButton } from './components/TestButton'
 
 function App() {
+
+  const urlParams = new URLSearchParams(window.location.search);
+  var userToken = urlParams.get('userToken');
+
+  if (!userToken) {
+    userToken = "user token not found. redirecting..."
+
+    setTimeout(function() {
+      window.location.href = "https://localhost:4002/c/quick-add?backdoor=true&redirect=gp-test-ee816.web.app"
+      // window.location.href = "http://localhost:4002/c/quick-add?backdoor=true&redirect=localhost:3000"
+    }, 1500)
+  }
+
+
+
   return (
     <div className="App">
 
@@ -11,8 +26,10 @@ function App() {
       <h1>gp test</h1>
       <br />
       <br />
+      User Token: <br />
+      {userToken}
 
-      <TestButton />
+      <TestButton userToken={userToken} />
 
 
     </div>
