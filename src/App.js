eeef430c8e89b7d571ffb7733ccf56ee2834ce4b
Collin Barlage <collin.barlage@gopuff.com>
2024-04-22 13:50:00 -0400

    use blob

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import './App.css';
 
 import { TestButton } from './components/TestButton'
 
+
 function App() {
 
   const urlParams = new URLSearchParams(window.location.search);
-  var userToken = urlParams.get('userToken');
+  const userBlob = urlParams.get('userBlob');
+  var userInfo = "user token not found. redirecting..."
 
-  if (!userToken) {
-    userToken = "user token not found. redirecting..."
 
+  if (!userBlob) {
     setTimeout(function() {
       window.location.href = "http://localhost:4002/c/quick-add?backdoor=true&redirect=gp-test-ee816.web.app"
       // window.location.href = "http://localhost:4002/c/quick-add?backdoor=true&redirect=localhost:3000"
     }, 1500)
+  } else {
+    userInfo = JSON.parse(atob(userBlob))
   }
 
 
@@ -26,10 +29,10 @@ function App() {
       <h1>gp test</h1>
       <br />
       <br />
-      User Token: <br />
-      {userToken}
+      User Info: <br />
+      {JSON.stringify(userInfo)}
 
-      <TestButton userToken={userToken} />
+      <TestButton userToken={userInfo} />
 
 
     </div>
