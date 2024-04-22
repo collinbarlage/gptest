5b7289511800e6205debff1233c37478649e55fb
Collin Barlage <collin.barlage@gopuff.com>
2024-04-22 17:25:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
 
   const urlParams = new URLSearchParams(window.location.search);
   const userBlob = urlParams.get('userBlob');
-  var userInfo = "user token not found. redirecting..."
+  var userInfo = "user token not found. redirecting...."
 
 
   if (!userBlob) {
     setTimeout(function() {
-      // window.location.href = "http://localhost:4002/c/quick-add?backdoor=true&redirect=gp-test-ee816.web.app"
-      window.location.href = "http://localhost:4002/c/quick-add?backdoor=true&redirect=localhost:3000"
+      window.location.href = "http://localhost:4002/c/quick-add?backdoor=true&redirect=gp-test-ee816.web.app"
+      // window.location.href = "http://localhost:4002/c/quick-add?backdoor=true&redirect=localhost:3000"
     }, 1500)
   } else {
     userInfo = JSON.parse(atob(userBlob))
@@ -32,7 +32,7 @@ function App() {
       User Info: <br />
       {JSON.stringify(userInfo)}
 
-      <CatchGame />
+      {userBlob && <CatchGame />}
 
 
     </div>
